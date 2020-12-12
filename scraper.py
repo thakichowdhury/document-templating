9d871943c9241f136a8c5aeefa303c64c9aaf4a7
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2020-12-12 12:45:08 -0800

    Delete extraneous functions from scraper, add function descriptions, and rename variables for clarity

--- scraper.py
+++ scraper.py
@@ -1,41 +1,29 @@
 import re
 
-def scrape_variables(s: str) -> list:
+def scrape_variables(template: str) -> list:
+    """
+    returns a list of the template variables inside the curly braces from input string s
+    """
     # find all substrings between pairs of square brackets { }
-    return re.findall(r'{([^}]*)}', s)
+    return re.findall(r'{([^}]*)}', template)
 
-# def scrape_defaults(l: list) -> dict:
-#     defaults: dict = {}
-
-#     # iterate through each string in input list of variables
-#     for v in l:
-#         # list destucture assign variable and default
-#         variable, default = decompose_variable_and_default(v)
-
-#         # to avoid overwriting a default with None, check if variable is NOT already in defaults before assigning
-#         if variable not in defaults:
-#             defaults[f'\{{v}\}'] = default
-
-#     return defaults
-
-def decompose_variable_and_default(s: str) -> list:
-    variable = s
+def decompose_variable_and_default(template_variable: str) -> list:
+    """
+    returns a list of the variable name and default value from the input template variable
+    """
+    variable = template_variable
     default = None
 
-    # if s has an equal sign, it means it has a default
+    # if template_variable has an equal sign, it means it has a default
     # find the index of the '=' in the string, else it will be -1
-    i = s.find('=')
+    i = template_variable.find('=')
 
     # if there's a default
     if i != -1:
         # find the variable (substring before the equal sign)
-        variable = s[:i]
+        variable = template_variable[:i]
         # find the default (substring after the equal sign)
-        default = s[i + 1:]
+        default = template_variable[i + 1:]
 
     return [variable, default]
 
-def scrape_variables_and_defaults(s: str) -> dict:
-    v = scrape_variables(s)
-    d = scrape_defaults(v)
-    return d
