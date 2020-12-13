28318353413e05de551006e1c3331275b6e8e591
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2020-12-13 02:47:17 -0800

    Change how os module is imported in helpers file

--- helpers.py
+++ helpers.py
@@ -1,13 +1,13 @@
 #! /usr/bin/env python3
 
-from os import path
+import os
 
 def get_file_dir(file_path: str) -> str:
-    return path.dirname(path.abspath(file_path))
+    return os.path.dirname(os.path.abspath(file_path))
 
 def get_abs_file_path(rel_path: str, file_path: str) -> str:
     abs_path: str = get_file_dir(file_path)
-    abs_file_path: str = path.join(abs_path, rel_path)
+    abs_file_path: str = os.path.join(abs_path, rel_path)
 
     return abs_file_path
 
