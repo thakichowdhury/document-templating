7144f472e1b393865b0eb030172325684a589b4b
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2020-12-13 02:32:37 -0800

    Add a helper function that generates command line menu options

--- helpers.py
+++ helpers.py
@@ -11,3 +11,24 @@ def get_abs_file_path(rel_path: str, file_path: str) -> str:
 
     return abs_file_path
 
+def generate_menu_options(options: list, menu_name = 'MENU') -> None:
+    border_char = '*'
+    border_width = 30
+    border_height = 5
+    s = int((border_width - len(menu_name))/2 - 3)
+
+    print(border_char * border_width)
+
+    for i in range(border_height - 2):
+        if i == (border_height - 3)/2:
+            print(border_char, ' ' * s, menu_name, ' ' * s, border_char)
+        else:
+            print(border_char, ' ' * (border_width - 4), border_char)
+
+    print(border_char * border_width)
+
+    for i in range(len(options)):
+        print(i, ':', options[i])
+
+    print('\n')
+
