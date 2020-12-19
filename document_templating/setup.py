107b1ce8ec48d82648c4a572fe0ed515312e10da
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2020-12-18 18:17:10 -0800

    Change template variable contact_name to hiring_manager to keep the example from getting swapped and formatted

--- document_templating/setup.py
+++ document_templating/setup.py
@@ -17,7 +17,7 @@ def create_example_template(dirname) -> str:
     example_template_path = f'{dirname}/example-template.txt'
 
     f = open(example_template_path, 'w')
-    f.write('This is an example template.\n\nYou can include a variable in your templates by enclosing the variable name in curly braces.\n\ne.g. Dear {contact_name}\n\nPlease remember to keep your variable names one word with no spaces. If you have to seperate words consider using "_" to demarcate.\n\nYou can even set default values for your template by appending them to your variable name with an "=".\n\ne.g. Dear {contact_name=Hiring Team}, I would love to work at {company_name} and have the opportunity to showcase my skills in {skills=development, system design, and much more}.\n')
+    f.write('This is an example template.\n\nYou can include a variable in your templates by enclosing the variable name in curly braces.\n\ne.g. Dear {contact_name}\n\nPlease remember to keep your variable names one word with no spaces. If you have to seperate words consider using "_" to demarcate.\n\nYou can even set default values for your template by appending them to your variable name with an "=".\n\ne.g. Dear {hiring_manager=Hiring Team}, I would love to work at {company_name} and have the opportunity to showcase my skills in {skills=development, system design, and much more}.\n')
     f.close()
 
     return example_template_path
