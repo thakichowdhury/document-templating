b264ff0d181d99acd53b9a6b8d6d5fe2c7097832
Thaki Chowdhury <thaki.chowdhury@gmail.com>
2020-12-18 18:15:30 -0800

    Add ANSI format codes to constants file

--- document_templating/constants.py
+++ document_templating/constants.py
@@ -3,4 +3,15 @@
 TEMPLATES_KEY = 'templates'
 TRANSFORMED_DOCUMENTS_KEY = 'transformed_documents'
 DATA_FILE = 'data.dat'
+ANSI_CODE = {
+    'HEADER': '\033[95m',
+    'OKBLUE': '\033[94m',
+    'OKCYAN': '\033[96m',
+    'OKGREEN': '\033[92m',
+    'WARNING': '\033[93m',
+    'FAIL': '\033[91m',
+    'ENDC': '\033[0m',
+    'BOLD': '\033[1m',
+    'UNDERLINE': '\033[4m',
+}
 
